ad1a923e6a1d5e7b330eb4973ef6b084a4da8e0f
Max Brovko <hellotakyon@gmail.com>
2024-11-10 22:52:24 +0300

    Add ability to add meta system config group for modules

---
+++ Block/Adminhtml/System/Config/Group/Meta.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace Takyon\Core\Block\Adminhtml\System\Config\Group;
+
+use Magento\Backend\Block\Context;
+use Magento\Backend\Model\Auth\Session;
+use Magento\Config\Block\System\Config\Form\Fieldset;
+use Magento\Framework\Phrase;
+use Magento\Framework\View\Helper\Js;
+use Magento\Framework\View\Helper\SecureHtmlRenderer;
+
+class Meta extends Fieldset
+{
+    public function __construct(
+        Context $context,
+        Session $authSession,
+        Js $jsHelper,
+        private string $version,
+        private string $documentationUrl,
+        array $data = [],
+        ?SecureHtmlRenderer $secureRenderer = null
+    ) {
+        parent::__construct($context, $authSession, $jsHelper, $data, $secureRenderer);
+    }
+
+    protected function _getHeaderTitleHtml($element): string
+    {
+        return '<a id="' . $element->getHtmlId() . '-head">' . $element->getLegend() . '</a>';
+    }
+
+    protected function _getExtraJs($element): string
+    {
+        return '';
+    }
+
+    protected function _getFooterHtml($element): string
+    {
+        $html = sprintf(
+            '%s: %s<br />%s%s',
+            __('Version'),
+            $this->version,
+            $this->getLinkHtml(__('Documentation'), $this->documentationUrl),
+            $this->getLinkHtml(__('Contact Support'), 'https://takyoncommerce.com/support')
+        );
+
+        return $html . parent::_getFooterHtml($element);
+    }
+
+    private function getLinkHtml(Phrase $label, string $url): string
+    {
+        return sprintf('<a href="%s" class="takyon-meta-fieldset__link">%s</a>', $url, $label);
+    }
+}
